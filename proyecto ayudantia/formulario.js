2db51e9309c699e7efdb155ce43f66edae2a3779
Dixon <pino@rayoapp.com>
2021-10-11 10:22:14 -0300

    Se Agrega el Proyecto

---
+++ proyecto ayudantia/formulario.js
@@ -0,0 +1,12 @@
+const nombre = document.getElementById("nombre")
+const correo = document.getElementById("correo")
+const form = document.getElementById("formulario")
+const boton = document.getElementById("boton");
+
+   
+
+
+boton.addEventListener("click",() =>{
+    
+    alert("se ha enviado tu consulta");
+})
